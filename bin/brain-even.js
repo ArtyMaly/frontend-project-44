6c97d4adc1f2236527eb601ed9db311375c1cf92
ArtyMaly <artemmaly@mail.ru>
2024-03-13 11:05:53 +0300

    ZASVO

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,19 +4,7 @@
 import gameBody from '../src/gameBody.js';
 
 // const userName = greeting();
+import task from '../src/even.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-const task = () => {
-  const q = Math.round(Math.random() * 100);
-
-  let a = '';
-  if (q % 2 === 0) {
-    a = 'yes';
-  } else {
-    a = 'no';
-  }
-
-  return [q, a];
-};
 gameBody(description, task);
---
+++ src/prime.js
@@ -0,0 +1,11 @@
+const task = () => {
+  const num = Math.round(Math.random() * 100);
+  const numD = Math.round(Math.sqrt(num));
+  for (let i = 2; i <= numD; i += 1) {
+    if (num % i === 0) {
+      return [num, 'no'];
+    }
+  }
+  return [num, 'yes'];
+};
+export default task;
